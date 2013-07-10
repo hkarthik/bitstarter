e4bc14814692db470d3164e7c5773222af98a28a
Karthik Hariharan <kar.hariharan@gmail.com>
2013-07-10 03:27:02 +0000

    Adding jshint fix.

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  fileBuffer = fs.readFileSync("index.html")
+  fileBuffer = fs.readFileSync("index.html");
   response.send(fileBuffer.toString());
 });
 
